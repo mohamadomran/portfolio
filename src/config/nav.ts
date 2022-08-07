050e21341a0b0e423d89368191959214cdd7ed11
Mohamad Omran <mohamad.omran@postlight.com>
2022-08-07 04:59:21 +0300

    feat: import new components

---
+++ src/config/nav.ts
@@ -0,0 +1,6 @@
+import { SOCIAL } from "@/config/social";
+
+export const NAV = [
+    { label: "Home", href: "/" },
+    { label: "Contact", href: `mailto:${SOCIAL.EMAIL}` },
+];
---
+++ src/config/social.tsx
@@ -0,0 +1,16 @@
+import { FaTwitter } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
+import { FiGithub } from "react-icons/fi";
+
+export const SOCIAL = {
+  GITHUB_ACCOUNT_URL: "https://github.com/mohamadomran",
+  EMAIL: "mohamadomran_13@hotmail.com",
+  INSTAGRAM_ACCOUNT_URL: "https://www.instagram.com/mohamadomran_13/",
+  TWITTER_ACCOUNT_URL : "https://twitter.com/itsmohamadomran",
+};
+
+export const HEADER_SOCIAL_ICONS = [
+  { href: SOCIAL.GITHUB_ACCOUNT_URL, "aria-label": "Github Account", icon: <FiGithub />, isExternal: true },
+  { href: SOCIAL.TWITTER_ACCOUNT_URL, "aria-label": "Medium Account", icon: <FaTwitter />, isExternal: true },
+  { href: SOCIAL.INSTAGRAM_ACCOUNT_URL, "aria-label": "Github Account", icon: <FaInstagram />, isExternal: true },
+];
