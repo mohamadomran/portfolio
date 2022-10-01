238372a3f412b83b9cdebed35eafc7cc5c46dadf
Mohamad Omran <mohamad.omran@postlight.com>
2022-10-01 23:39:55 +0300

    fix: remove garden link

--- src/config/nav.ts
+++ src/config/nav.ts
@@ -2,6 +2,5 @@ import { SOCIAL } from "@/config/social";
 
 export const NAV = [
     { label: "Home", href: "/" },
-    { label: "Digital Garden", href: "/garden" },
     { label: "Contact", href: `mailto:${SOCIAL.EMAIL}` },
 ];
