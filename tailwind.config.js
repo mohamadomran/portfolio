6271171ff11bd84248fe411a85deac05a468060d
Mohamad Omran <mohamad.omran@postlight.com>
2023-03-14 02:04:55 +0200

    fix: tailwind modification

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,32 +9,31 @@ module.exports = {
     theme: {
         extend: {
             colors: {
-                teal: {
-                    500: '#70FFF8'
+                green: {
+                    950: '#1fdf64'
                 },
                 black: {
-                    700: '#1C1E21',
                     950: '#0a0a0a'
                 }
             }
         },
         fontFamily: {
             mono: ['JetBrains Mono', 'monospace'],
-          },
+        },
         fontSize: {
-            xs: ['0.8125rem', {lineHeight: '1.5rem'}],
-            sm: ['0.875rem', {lineHeight: '1.5rem'}],
-            base: ['1rem', {lineHeight: '1.75rem'}],
-            lg: ['1.125rem', {lineHeight: '1.75rem'}],
-            xl: ['1.25rem', {lineHeight: '2rem'}],
-            '2xl': ['1.5rem', {lineHeight: '2rem'}],
-            '3xl': ['1.875rem', {lineHeight: '2.25rem'}],
-            '4xl': ['2rem', {lineHeight: '2.5rem'}],
-            '5xl': ['3rem', {lineHeight: '3.5rem'}],
-            '6xl': ['3.75rem', {lineHeight: '1'}],
-            '7xl': ['4.5rem', {lineHeight: '1'}],
-            '8xl': ['6rem', {lineHeight: '1'}],
-            '9xl': ['8rem', {lineHeight: '1'}],
+            xs: ['0.8125rem', { lineHeight: '1.5rem' }],
+            sm: ['0.875rem', { lineHeight: '1.5rem' }],
+            base: ['1rem', { lineHeight: '1.75rem' }],
+            lg: ['1.125rem', { lineHeight: '1.75rem' }],
+            xl: ['1.25rem', { lineHeight: '2rem' }],
+            '2xl': ['1.5rem', { lineHeight: '2rem' }],
+            '3xl': ['1.875rem', { lineHeight: '2.25rem' }],
+            '4xl': ['2rem', { lineHeight: '2.5rem' }],
+            '5xl': ['3rem', { lineHeight: '3.5rem' }],
+            '6xl': ['3.75rem', { lineHeight: '1' }],
+            '7xl': ['4.5rem', { lineHeight: '1' }],
+            '8xl': ['6rem', { lineHeight: '1' }],
+            '9xl': ['8rem', { lineHeight: '1' }],
         },
         typography: (theme) => ({
             invert: {
@@ -67,7 +66,7 @@ module.exports = {
                     '--tw-prose-headings': theme('colors.zinc.900'),
                     '--tw-prose-links': theme('colors.indigo.500'),
                     '--tw-prose-links-hover': theme('colors.indigo.600'),
-                    '--tw-prose-underline': theme('colors.zinc.500 / 0.2'),
+                    '--tw-prose-underline': theme('colors.teal.500 / 0.2'),
                     '--tw-prose-underline-hover': theme('colors.indigo.500'),
                     '--tw-prose-bold': theme('colors.zinc.900'),
                     '--tw-prose-counters': theme('colors.zinc.900'),
