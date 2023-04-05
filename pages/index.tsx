51a2642e84bb0c978ee4f5bb38e63a3d6fe3fee2
Mohamad Omran <mohamad.omran@postlight.com>
2023-04-06 01:29:13 +0300

    fix: refactored header

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,81 @@
 import React from 'react';
 import Head from 'next/head';
 import Image from 'next/image';
+
 import { Container } from '@/components/Container';
 import {
-GitHubIcon,
-LinkedInIcon,
-TwitterIcon,
+    GitHubIcon,
+    LinkedInIcon,
+    TwitterIcon,
 } from '@/components/icons/SocialIcons';
 import { MailIcon } from '@/components/icons/MailIcon';
-
 import { SocialLink } from '@/components/ui/SocialLink';
+import { ArrowDownIcon } from '@/components/icons/ArrowDownIcon';
+import { Button } from '@/components/ui/Button';
 
 import { SEO_DESCRIPTION, SEO_TITLE, SOCIAL_LINKS } from '@/Utils/constants';
 import avatar from '@/public/static/images/avatar.png';
-import { Button } from '@/components/ui/Button';
-import { ArrowDownIcon } from '@/components/icons/ArrowDownIcon';
+
+const Avatar = () => (
+    <div className='md:mt-10 md:pr-10'>
+        <div className='mx-auto max-w-xs px-2.5 lg:mx-0 lg:max-w-none'>
+            <Image
+                src={avatar}
+                alt=''
+                sizes='(min-width: 1024px) 32rem, 20rem'
+                className='aspect-square w-full rounded-full border-4 border-mustard-500 bg-zinc-100 object-cover shadow-inner dark:bg-zinc-800'
+                placeholder='blur'
+            />
+        </div>
+    </div>
+);
+
+const Intro = () => (
+    <div className='max-w-3xl'>
+        <h1 className='dark:sky-300 bg-gradient-to-r from-mustard-500 to-mustard-300 bg-clip-text text-center text-5xl font-bold text-zinc-800 dark:text-transparent sm:text-left md:text-5xl mt-10'>
+            Hi, I&apos;m Mohamad!
+        </h1>
+        <p className='mt-4 max-w-2xl text-base text-zinc-600 dark:text-white sm:mt-10 text-center md:text-left'>
+            As an experienced full-stack software engineer specializing in AI
+            and machine learning, I am committed to developing innovative
+            solutions to complex problems, and bringing fresh ideas to life
+            through technology. Outside of work, I enjoy exploring new
+            destinations with my camera, capturing the beauty of the world
+            through photography. Whether you&apos;re seeking a skilled developer
+            or just curious to peruse my portfolio, I welcome you to explore my
+            work and learn how I can help transform your ideas into tangible
+            outcomes
+        </p>
+        <div className='flex items-center gap-6'>
+            <SocialLink
+                href={SOCIAL_LINKS.GITHUB}
+                ariaLabel='Follow on GitHub'
+                icon={GitHubIcon}
+            />
+            <SocialLink
+                href={SOCIAL_LINKS.LINKEDIN}
+                ariaLabel='Follow on LinkedIn'
+                icon={LinkedInIcon}
+            />
+            <SocialLink
+                href={SOCIAL_LINKS.TWITTER}
+                ariaLabel='Follow on Twitter'
+                icon={TwitterIcon}
+            />
+            <SocialLink
+                href={SOCIAL_LINKS.EMAIL}
+                ariaLabel='Send me an Email'
+                icon={MailIcon}
+            />
+            <Button
+                href={SOCIAL_LINKS.CV}
+                className='text-white-100 group mt-6 ring ring-mustard-300 hover:bg-mustard-500 hover:text-black-950 dark:hover:bg-mustard-500'>
+                Download CV
+                <ArrowDownIcon className='h-4 w-4 stroke-zinc-400 group-active:stroke-zinc-600 dark:group-hover:stroke-zinc-50 dark:group-active:stroke-zinc-50' />
+            </Button>
+        </div>
+    </div>
+);
 
 export default function Home() {
     return (
@@ -44,66 +105,10 @@ export default function Home() {
                 />
             </Head>
 
-            <Container className='mt-9'>
-                <div className='grid grid-cols-1 gap-y-16 lg:grid-cols-2 lg:grid-rows-[auto_1fr] lg:gap-y-12'>
-                    <div className='lg:pr-20 lg:mt-10'>
-                        <div className='max-w-xs px-2.5 lg:max-w-none'>
-                            <Image
-                                src={avatar}
-                                alt=''
-                                sizes='(min-width: 1024px) 32rem, 20rem'
-                                className='aspect-square shadow-inner rounded-full bg-zinc-100 object-cover dark:bg-zinc-800 border-4 border-mustard-500'
-                                placeholder='blur'
-                            />
-                        </div>
-                    </div>
-
-                    <div className='max-w-4xl'>
-                        <h1 className='text-5xl font-bold text-zinc-800 dark:sky-300 sm:text-5xl bg-clip-text dark:text-transparent bg-gradient-to-r from-mustard-500 to-mustard-300'>
-                            Hi, I&apos;m Mohamad!
-                        </h1>
-                        <p className='max-w-2xl text-l mt-6 text-base text-zinc-600 dark:text-white'>
-                            As an experienced full-stack software engineer
-                            specializing in AI and machine learning, I am
-                            committed to developing innovative solutions to
-                            complex problems, and bringing fresh ideas to life
-                            through technology. Outside of work, I enjoy
-                            exploring new destinations with my camera, capturing
-                            the beauty of the world through photography. Whether
-                            you&apos;re seeking a skilled developer or just
-                            curious to peruse my portfolio, I welcome you to
-                            explore my work and learn how I can help transform
-                            your ideas into tangible outcomes
-                        </p>
-                        <div className='flex gap-6 items-center'>
-                            <SocialLink
-                                href={SOCIAL_LINKS.GITHUB}
-                                ariaLabel='Follow on GitHub'
-                                icon={GitHubIcon}
-                            />
-                            <SocialLink
-                                href={SOCIAL_LINKS.LINKEDIN}
-                                ariaLabel='Follow on LinkedIn'
-                                icon={LinkedInIcon}
-                            />
-                            <SocialLink
-                                href={SOCIAL_LINKS.TWITTER}
-                                ariaLabel='Follow on Twitter'
-                                icon={TwitterIcon}
-                            />
-                            <SocialLink
-                                href={SOCIAL_LINKS.EMAIL}
-                                ariaLabel='Send me an Email'
-                                icon={MailIcon}
-                            />
-                            <Button
-                                href={SOCIAL_LINKS.CV}
-                                className='group mt-6 hover:bg-mustard-500 dark:hover:bg-mustard-500 text-white-100 hover:text-black-950 ring ring-mustard-300'>
-                                Download CV
-                                <ArrowDownIcon className='h-4 w-4 stroke-zinc-400 group-active:stroke-zinc-600 dark:group-hover:stroke-zinc-50 dark:group-active:stroke-zinc-50' />
-                            </Button>
-                        </div>
-                    </div>
+            <Container className='mx-4 mt-9'>
+                <div className='grid grid-cols-1 md:grid-cols-2 md:grid-rows-[auto_1fr]'>
+                    <Avatar />
+                    <Intro />
                 </div>
             </Container>
         </>
