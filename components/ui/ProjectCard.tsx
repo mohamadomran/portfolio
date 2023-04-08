1a972a0f9f79fa58f1b7c70d654aaa2d8ca6a0d5
Mohamad Omran <mohamad.omran@postlight.com>
2023-04-09 00:02:45 +0300

    feat: featured project card component

---
+++ components/ui/ProjectCard.tsx
@@ -0,0 +1,33 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
+import { PortableText } from '@portabletext/react';
+import { Project } from '@/types/Project'
+
+export const ProjectCard = ({ project }: { project: Project }) => {
+    const { name, image, url, content } = project;
+
+    return (
+        <div className='card card-compact shadow-xl mt-8 bg-base-100'>
+            <Image
+                src={image}
+                alt={name}
+                width={400}
+                height={300}
+                className='aspect-rectangle rounded-t-lg w-full h-full object-cover'
+            />
+            <div className='card-body'>
+                <h2 className='card-title'>{name}</h2>
+                <PortableText value={content} />
+                <div className='card-actions justify-end mt-4'>
+                    <Link
+                        className='btn btn-primary'
+                        href={url}
+                        target='_blank'>
+                        Check it out
+                    </Link>
+                </div>
+            </div>
+        </div>
+    );
+};
