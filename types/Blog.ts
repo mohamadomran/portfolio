26d5c705f5f13aa177073d8a9030e7582b601f6d
Mohamad Omran <mohamad.omran@postlight.com>
2023-04-09 02:37:51 +0300

    feat: add blocks

---
+++ types/Blog.ts
@@ -0,0 +1,12 @@
+import { PortableTextBlock } from 'sanity';
+
+export type Blog = {
+    _id: string;
+    _createdAt: Date;
+    title: string;
+    slug: {
+        current: string;
+    };
+    image: string;
+    content: PortableTextBlock[];
+};
