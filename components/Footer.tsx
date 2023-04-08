05642c446563b2ad32c720c45d50d17954daa42f
Mohamad Omran <mohamad.omran@postlight.com>
2023-04-09 02:40:33 +0300

    fix: cleanup footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,29 +1,14 @@
-import React from 'react';
-import { OuterContainer, InnerContainer } from './Container';
-import { SOCIAL_LINKS } from '@/Utils/constants';
-import { GitHubIcon, LinkedInIcon, TwitterIcon } from './icons/SocialIcons';
-import { SocialLink } from '@/components/ui/SocialLink';
-import { ArrowDownIcon } from './icons/ArrowDownIcon';
-import { Button } from './ui/Button';
-
 export function Footer() {
     return (
-        <footer className='mt-16'>
-            <OuterContainer>
-                <div className='border-t border-secondary pb-8'>
-                    <InnerContainer>
-                        <div className='flex flex-col items-center justify-between gap-6 mt-12'>
-
-                            <div className='flex flex-col'>
-                                <p className='text-sm text-secondary'>
-                                    &copy; {new Date().getFullYear()} Mohamad
-                                    Omran. All rights reserved.
-                                </p>
-                            </div>
-                        </div>
-                    </InnerContainer>
+        <footer className='mt-8 mb-8'>
+            <div className='flex flex-col items-center justify-between gap-6 mt-12 '>
+                <div className='flex flex-col'>
+                    <p className='text-sm text-base-content'>
+                        &copy; {new Date().getFullYear()} Mohamad Omran. All
+                        rights reserved.
+                    </p>
                 </div>
-            </OuterContainer>
+            </div>
         </footer>
     );
 }
