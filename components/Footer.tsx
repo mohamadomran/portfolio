99244f0f8c4e05c0f67d620993665e05936413bf
Mohamad Omran <mohamad.omran@postlight.com>
2023-04-08 05:38:24 +0300

    feat: update footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,28 +1,38 @@
 import React from 'react';
 import { OuterContainer, InnerContainer } from './Container';
-import { NavLink } from '@/components/ui/Navigation';
+import { SOCIAL_LINKS } from '@/Utils/constants';
+import { GitHubIcon, LinkedInIcon, TwitterIcon } from './icons/SocialIcons';
+import { SocialLink } from '@/components/ui/SocialLink';
 
 export function Footer() {
     return (
-        <footer className='mt-32'>
+        <footer className='mt-16'>
             <OuterContainer>
-                <div className='border-t border-zinc-100 pb-16 dark:border-zinc-700/40'>
+                <div className='border-t border-secondary pb-8'>
                     <InnerContainer>
                         <div className='flex flex-col items-center justify-between gap-6 mt-12'>
-                            <div className='flex flex-wrap justify-center gap-6 text-sm font-medium text-zinc-800 dark:text-zinc-200'>
-                                <NavLink href='/'>Home</NavLink>
-                                <NavLink href='/about'>About</NavLink>
-                                <NavLink href='/work'>Work</NavLink>
-                                <NavLink href='/writing'>Writing</NavLink>
-                                <NavLink href='/projects'>Projects</NavLink>
-                                {/* <NavLink href='/snippets'>Snippets</NavLink> */}
+                            <div className='flex items-center gap-6'>
+                                <SocialLink
+                                    href={SOCIAL_LINKS.GITHUB}
+                                    ariaLabel='Follow on GitHub'
+                                    icon={GitHubIcon}
+                                />
+                                <SocialLink
+                                    href={SOCIAL_LINKS.LINKEDIN}
+                                    ariaLabel='Follow on LinkedIn'
+                                    icon={LinkedInIcon}
+                                />
+                                <SocialLink
+                                    href={SOCIAL_LINKS.TWITTER}
+                                    ariaLabel='Follow on Twitter'
+                                    icon={TwitterIcon}
+                                />
                             </div>
                             <div className='flex flex-col'>
-                                <p className='text-sm text-zinc-600 dark:text-zinc-400'>
+                                <p className='text-sm text-secondary'>
                                     &copy; {new Date().getFullYear()} Mohamad
                                     Omran. All rights reserved.
                                 </p>
-
                             </div>
                         </div>
                     </InnerContainer>
