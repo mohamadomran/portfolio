5d3b3fd224484747b5db315ad58d217b8f861951
Mohamad Omran <mohamad.omran@postlight.com>
2023-04-09 05:40:36 +0300

    feat: show projects

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -1,79 +1,64 @@
-import { GetStaticProps } from 'next'
-import Head from 'next/head'
-import { Card } from '@/components/ui/Card'
-import { SimpleLayout } from '@/components/layouts/SimpleLayout'
-import { SparklesIcon } from '@/components/icons/SparklesIcon'
-import { ShareIcon } from '@/components/icons/ShareIcon'
-import { getPinnedRepos } from '@/lib/github'
-import { numberFormat } from '@/lib/numberFormat'
-import type { Repo } from '@/types/icons';
+import { GetServerSideProps } from 'next';
+import Head from 'next/head';
+import Image from 'next/image'
+import { getProjects } from '@/utils/sanity-utils';
+import { SimpleLayout } from '@/components/layouts/SimpleLayout';
+import Link from 'next/link';
+import { Project } from '@/types/Project';
+import { PortableText } from '@portabletext/react';
 
-export default function Projects({ pinnedRepos }: { pinnedRepos: Repo[] }) {
+export default function FeaturedProjects({ projects }: { projects: Project[]}) {
     return (
         <>
             <Head>
-                <title>Projects - Mohamad Omran</title>
+                <title>Featured Projects - Mohamad Omran</title>
                 <meta
-                    name="description"
-                    content="Open Source Projects/Tinkering: My Contributions to the Community"
+                    name='description'
+                    content='Featured Projects'
                 />
+                <meta property='og:title' content='Projects - Mohamad Omran' />
                 <meta
-                    property="og:title"
-                    content="Projects - Mohamad Omran"
-                />
-                <meta
-                    property="og:description"
-                    content="Open Source Projects/Tinkering: My Contributions to the Community"
+                    property='og:description'
+                    content='Featured Projects'
                 />
             </Head>
             <SimpleLayout
-                title="Open Source Projects/Tinkering: My Contributions to the Community"
-                gradient="bg-gradient-to-r from-sky-400 to-blue-500"
-            >
-                <ul
-                    role="list"
-                    className="grid grid-cols-1 gap-x-12 gap-y-16 sm:grid-cols-2 lg:grid-cols-3"
-                >
-                    {pinnedRepos.map((repo) => (
-                        <Card as="li" key={repo.name}>
-                            <h2 className="text-base font-semibold transition group-hover:text-indigo-500 text-zinc-800 dark:text-zinc-100">
-                                <Card.Link href={repo.url}>{repo.name}</Card.Link>
-                            </h2>
-                            <Card.Description>{repo.description}</Card.Description>
-                            <div
-                                className="z-10 flex space-x-16 sm:space-x-0 sm:justify-between mt-8 items-center w-full group text-sm text-zinc-500 dark:text-zinc-400">
-                                <p
-                                    className="flex items-center">
-                                    <span>{repo.primaryLanguage.name}</span>
-                                    <span
-                                        className="mr-2 w-4 h-4 rounded-full order-first"
-                                        style={{ backgroundColor: repo.primaryLanguage.color }} />
-                                </p>
-                                <div className="flex space-x-6">
-                                    <p className="flex items-center">
-                                        {numberFormat(repo.stargazerCount)}
-                                        <SparklesIcon className="order-first mr-2 w-5 h-5 fill-zinc-400 dark:fill-zinc-500" />
-                                    </p>
-                                    <p className="flex items-center">
-                                        {numberFormat(repo.forkCount)}
-                                        <ShareIcon
-                                            className="order-first mr-2 w-5 h-5 fill-zinc-400 dark:fill-zinc-500 -rotate-90" />
-                                    </p>
-                                </div>
+                title='Featured Projects'
+                gradient='bg-gradient-to-r from-sky-400 to-blue-500'>
+                <div className='mt-5 grid gap-8 md:grid-cols-2 lg:grid-cols-3'>
+                    {projects.map((project: Project) => (
+                        <Link
+                            href={project.url}
+                            key={project._id}
+                            className='rounded-lg border-2 border-gray-500 p-1 transition hover:scale-105 hover:border-blue-500'>
+                            {project.image && (
+                                <Image
+                                    src={project.image}
+                                    alt={project.name}
+                                    width={750}
+                                    height={300}
+                                    className='rounded-lg border border-gray-500 object-cover'
+                                />
+                            )}
+                            <div className='mt-2 bg-gradient-to-r from-orange-400 via-red-500 to-purple-600 bg-clip-text font-extrabold text-transparent'>
+                                {project.name}
                             </div>
-                        </Card>
+
+                            <div className="text-lg text-gray-700 mt-5">
+                                <PortableText value={project.content} />
+                            </div>
+                        </Link>
                     ))}
-                </ul>
+                </div>
             </SimpleLayout>
         </>
-    )
+    );
 }
 
-export const getStaticProps: GetStaticProps = async () => {
+export const getServerSideProps: GetServerSideProps = async () => {
     return {
         props: {
-            pinnedRepos: (await getPinnedRepos())
-                .sort((a, b) => b.stargazerCount - a.stargazerCount)
+            projects: (await getProjects()) || [],
         }
     }
 }
