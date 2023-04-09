913079fb5891f34b6573596c7383748c6d2c395b
Mohamad Omran <mohamad.omran@postlight.com>
2023-04-09 05:40:09 +0300

    feat: add about is seo title

--- Utils/constants.ts
+++ Utils/constants.ts
@@ -1,8 +1,12 @@
 export const SEO_TITLE = 'Portfolio / Mohamad Omran';
+export const ABOUT_SEO_TITLE = 'About / Mohamad Omran';
 export const WORK_SEO_TITLE = 'Work Experience / Mohamad Omran';
 export const BLOG_SEO_TITLE = 'Blog / Mohamad Omran';
 
-export const SEO_DESCRIPTION = 'Explore my fullstack portfolio showcasing AI and machine learning projects. With a passion for photography and travel, I approach problem-solving with creativity and attention to detail. Discover the power of technology in solving complex problems and bringing new ideas to life.';
+export const SEO_DESCRIPTION =
+    'Explore my fullstack portfolio showcasing AI and machine learning projects. With a passion for photography and travel, I approach problem-solving with creativity and attention to detail. Discover the power of technology in solving complex problems and bringing new ideas to life.';
+
+export const ABOUT_SEO_DESCRIPTION = `Learn more about my background, experience, and passion for software engineering. Explore my story, discover my expertise, and see why I'm the right choice for your next project`;
 export const WORK_SEO_DESCRIPTION = `Explore my fullstack portfolio showcasing AI and machine learning projects. With a passion for photography and travel, I approach problem-solving with creativity and attention to detail. Discover the power of technology in solving complex problems and bringing new ideas to life.`;
 export const BLOG_SEO_DESCRIPTION = `My insights and commentary on the latest tech trends and topics on my personal blog. From in-depth analyses to personal stories, I share my thoughts on a wide range of tech-related topics`;
 
