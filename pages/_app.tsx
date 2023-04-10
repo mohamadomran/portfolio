b95fff9216a82f4a4d5a20c36409a5b3ad170921
Mohamad Omran <mohamad.omran@postlight.com>
2023-04-10 15:20:04 +0300

    feat: updated layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,13 @@
-import type { AppProps } from 'next/app';
-import { Header } from '@/components/Header';
-import { Footer } from '@/components/Footer';
-import { Analytics } from '@vercel/analytics/react';
-
 import '../styles/tailwind.css';
+
+import { Analytics } from '@vercel/analytics/react';
+import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Header />
-      <main className="px-10 sm:px-11 md:px-12 py-5 sm:py-6 md:py-0 max-w-3xl mx-auto w-screen">
-        <Component {...pageProps} />
-        <Analytics />
-      </main>
-      <Footer />
+      <Component {...pageProps} />
+      <Analytics />
     </>
   );
 }
