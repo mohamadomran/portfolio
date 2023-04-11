c5d0e5d7a8ca23ce4c0c2dc6817043caa94f957b
Mohamad Omran <mohamad.omran@postlight.com>
2023-04-12 02:59:32 +0300

    feat: gallery placeholder page

---
+++ pages/gallery.tsx
@@ -0,0 +1,11 @@
+import { Layout } from '@/components/layouts/Layout';
+
+export default function Work() {
+  return (
+    <Layout>
+      <div className="my-6 flex h-[70vh] flex-col items-center justify-center">
+        Comming Soon 😅
+      </div>
+    </Layout>
+  );
+}
