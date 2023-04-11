73d5b8b8502d69bf21b0bd709b0f4f6e947011d7
Mohamad Omran <mohamad.omran@postlight.com>
2023-04-11 03:56:24 +0300

    feat: projects page

---
+++ components/layouts/Grid_Layout.tsx
@@ -0,0 +1,18 @@
+import { Footer } from '@/components/Footer';
+import { Header } from '@/components/Header';
+
+type GridLayoutProps = {
+  children: React.ReactNode;
+};
+
+export const GridLayout = ({ children }: GridLayoutProps) => {
+  return (
+    <>
+      <Header />
+      <main className="mx-auto w-screen max-w-6xl px-10 py-5 sm:px-11 sm:py-6 md:px-12 md:py-0">
+        {children}
+      </main>
+      <Footer />
+    </>
+  );
+};
--- pages/projects.tsx
+++ pages/projects.tsx
@@ -1,10 +1,11 @@
 import { PortableText } from '@portabletext/react';
+import { urlForImage } from 'lib/sanity.image';
 import { GetServerSideProps } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
 import Link from 'next/link';
 
-import { Layout } from '@/components/layouts/Layout';
+import { GridLayout } from '@/components/layouts/Grid_Layout';
 import { getAllProjects } from '@/lib/sanity.client';
 import { Project } from '@/types/Project';
 
@@ -14,40 +15,42 @@ export default function FeaturedProjects({
   projects: Project[];
 }) {
   return (
-    <Layout>
+    <GridLayout>
       <Head>
         <title>Featured Projects - Mohamad Omran</title>
         <meta name="description" content="Featured Projects" />
         <meta property="og:title" content="Projects - Mohamad Omran" />
         <meta property="og:description" content="Featured Projects" />
       </Head>
-      {/* <div className="mt-5 grid gap-8 md:grid-cols-2 lg:grid-cols-3">
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
         {projects.map((project: Project) => (
-          <Link
-            href={project.url}
-            key={project._id}
-            className="rounded-lg border-2 border-gray-500 p-1 transition hover:scale-105 hover:border-blue-500"
-          >
-            {project.image && (
-              <Image
-                src={project.image}
-                alt={project.name}
-                width={750}
-                height={300}
-                className="rounded-lg border border-gray-500 object-cover"
-              />
-            )}
-            <div className="mt-2 bg-gradient-to-r from-orange-400 via-red-500 to-purple-600 bg-clip-text font-extrabold text-transparent">
-              {project.name}
-            </div>
+          <div key={project._id} className="mt-8 shadow-sm card rounded-xl bg-primary-content">
+            <Image
+              src={urlForImage(project.coverImage).height(1000).width(2000).url()}
+              alt={`Cover Image for ${project.title}`}
+              width={400}
+              height={300}
+              className="aspect-rectangle w-full h-full rounded-t-lg object-cover"
+            />
+            <div className="card-body">
+              <h2 className="card-title">{project.title}</h2>
 
-            <div className="mt-5 text-lg text-gray-700">
-              <PortableText value={project.content} />
+              <PortableText value={project.description[0]} />
+
+              <div className="card-actions mt-4 justify-end">
+                <Link
+                  className="btn-outline btn-primary btn"
+                  href={project.url!}
+                  target="_blank"
+                >
+                  Check it out
+                </Link>
+              </div>
             </div>
-          </Link>
+          </div>
         ))}
-      </div> */}
-    </Layout>
+      </div>
+    </GridLayout >
   );
 }
 
