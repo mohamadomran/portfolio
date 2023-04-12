b7021050330d509fc963b411ed85966b7118f94b
Mohamad Omran <mohamad.omran@postlight.com>
2023-04-12 13:15:44 +0300

    fix: undefined title

--- components/pages/blog/HeroPost.tsx
+++ components/pages/blog/HeroPost.tsx
@@ -11,14 +11,16 @@ export default function HeroPost(
   return (
     <section>
       <div className="mb-8 md:mb-16">
-        <CoverImage slug={slug} title={title!} image={coverImage} priority />
+        <CoverImage slug={slug} title={title!} image={coverImage!} priority />
       </div>
       <div className="mb-20 md:mb-28 md:grid md:grid-cols-2 md:gap-x-16 lg:gap-x-8">
         <div>
           <h3 className="mb-4 text-4xl leading-tight lg:text-6xl">
-            <Link href={`/blog/${slug}`} className="hover:underline">
-              {title || 'Untitled'}
-            </Link>
+            {title && (
+              <Link href={`/blog/${slug}`} className="hover:underline">
+                {title}
+              </Link>
+            )}
           </h3>
 
           <Date dateString={date!} />
