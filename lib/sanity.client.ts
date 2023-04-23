9ff7bf560893f2510f41dfea01d33fbc21456ea0
Mohamad Omran <mohamad.omran@postlight.com>
2023-04-23 13:46:42 +0300

    chore: updated APIs

--- lib/sanity.client.ts
+++ lib/sanity.client.ts
@@ -1,87 +1,57 @@
-import { apiVersion, dataset, projectId, useCdn } from 'lib/sanity.api';
-import {
-  allBlogsQuery,
-  allProjectsQuery,
-  featuredBlogsQuery,
-  featuredProjectsQuery,
-  galleryQuery,
-  postBySlugQuery,
-  postSlugsQuery,
-  settingsQuery,
-} from 'lib/sanity.queries';
+import 'server-only';
+
 import { createClient } from 'next-sanity';
 
-import { type Post } from '@/types/Post';
-import { type Project } from '@/types/Project';
-import { type Settings } from '@/types/Settings';
+import { apiVersion, dataset, projectId, useCdn } from '@/lib/sanity.api';
+import {
+  galleryPageQuery,
+  homePageQuery,
+  pagesBySlugQuery,
+  settingsQuery,
+} from '@/lib/sanity.queries';
+import type {
+  BlogPagePayload,
+  GalleryPagePayload,
+  HomePagePayload,
+  SettingsPayload,
+} from '@/types';
 
-const client = projectId
-  ? createClient({ projectId, dataset, apiVersion, useCdn })
-  : null;
+export const sanityClient = (token?: string) => {
+  return projectId
+    ? createClient({ projectId, dataset, apiVersion, useCdn, token })
+    : null;
+};
 
-export async function getSettings(): Promise<Settings> {
-  if (client) {
-    return (await client.fetch(settingsQuery)) || {};
-  }
-  return {};
+export async function getHomePage({
+  token,
+}: {
+  token?: string;
+}): Promise<HomePagePayload | undefined> {
+  return await sanityClient(token)?.fetch(homePageQuery);
 }
 
-export async function getAllPosts(): Promise<Post[]> {
-  if (client) {
-    return (await client.fetch(featuredBlogsQuery)) || [];
-  }
-  return [];
+export async function getGalleryPage({
+  token,
+}: {
+  token?: string;
+}): Promise<GalleryPagePayload | undefined> {
+  return await sanityClient(token)?.fetch(galleryPageQuery);
 }
 
-export async function getFeaturedtProject(): Promise<Project[]> {
-  if (client) {
-    return (await client.fetch(featuredProjectsQuery)) || [];
-  }
-  return [];
+export async function getBlogBySlug({
+  slug,
+  token,
+}: {
+  slug: string;
+  token?: string;
+}): Promise<BlogPagePayload | undefined> {
+  return await sanityClient(token)?.fetch(pagesBySlugQuery, { slug });
 }
 
-export async function getAllProjects(): Promise<Project[]> {
-  if (client) {
-    return (await client.fetch(allProjectsQuery)) || [];
-  }
-  return [];
-}
-
-export async function getAllPostsSlugs(): Promise<Pick<Post, 'slug'>[]> {
-  if (client) {
-    const slugs = (await client.fetch<string[]>(postSlugsQuery)) || [];
-    return slugs.map((slug) => ({ slug }));
-  }
-  return [];
-}
-
-export async function getPostBySlug(slug: string): Promise<Post> {
-  if (client) {
-    return (await client.fetch(postBySlugQuery, { slug })) || ({} as any);
-  }
-  return {} as any;
-}
-
-export async function getPostAndMoreStories(
-  slug: string,
-  token?: string | null,
-): Promise<{ post: Post }> {
-  if (projectId) {
-    const client = createClient({
-      projectId,
-      dataset,
-      apiVersion,
-      useCdn,
-      token: token || undefined,
-    });
-    return await client.fetch(allBlogsQuery, { slug });
-  }
-  return { post: null as any };
-}
-
-export async function fetchGallery(): Promise<Post[]> {
-  if (client) {
-    return (await client.fetch(galleryQuery)) || [];
-  }
-  return [];
+export async function getSettings({
+  token,
+}: {
+  token?: string;
+}): Promise<SettingsPayload | undefined> {
+  return await sanityClient(token)?.fetch(settingsQuery);
 }
