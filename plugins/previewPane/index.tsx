9430e60ef5fabb48284b6855c1789f946dd3e793
Mohamad Omran <mohamad.omran@postlight.com>
2023-04-23 12:55:38 +0300

    fix: preview plugin to be for all pages/posts

--- plugins/previewPane/index.tsx
+++ plugins/previewPane/index.tsx
@@ -1,13 +1,7 @@
-// This plugin is responsible for adding a “Preview” tab to the document pane
-// You can add any React component to `S.view.component` and it will be rendered in the pane
-// and have access to content in the form in real-time.
-// It's part of the Studio's “Structure Builder API” and is documented here:
-// https://www.sanity.io/docs/structure-builder-reference
-
 import { DefaultDocumentNodeResolver } from 'sanity/desk';
-import postType from 'schemas/post';
 
-import PostPreviewPane from './PostPreviewPane';
+import { PREVIEWABLE_DOCUMENT_TYPES } from '../../sanity.config';
+import { PreviewPane } from './PreviewPane';
 
 export const previewDocumentNode = ({
   apiVersion,
@@ -17,23 +11,23 @@ export const previewDocumentNode = ({
   previewSecretId: `${string}.${string}`;
 }): DefaultDocumentNodeResolver => {
   return (S, { schemaType }) => {
-    switch (schemaType) {
-      case postType.name:
-        return S.document().views([
-          S.view.form(),
-          S.view
-            .component(({ document }) => (
-              <PostPreviewPane
-                slug={document.displayed.slug?.current}
-                apiVersion={apiVersion}
-                previewSecretId={previewSecretId}
-              />
-            ))
-            .title('Preview'),
-        ]);
-
-      default:
-        return null;
+    if (PREVIEWABLE_DOCUMENT_TYPES.includes(schemaType)) {
+      return S.document().views([
+        // Default form view
+        S.view.form(),
+        // Preview
+        S.view
+          .component((props) => (
+            <PreviewPane
+              previewSecretId={previewSecretId}
+              apiVersion={apiVersion}
+              {...props}
+            />
+          ))
+          .title('Preview'),
+      ]);
     }
+
+    return null;
   };
 };
