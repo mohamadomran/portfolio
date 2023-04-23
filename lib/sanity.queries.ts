a3dd440248433e6e8cfe325be3505e298699fa4b
Mohamad Omran <mohamad.omran@postlight.com>
2023-04-23 13:49:05 +0300

    feat: updated queries to reflect changes in schema

--- lib/sanity.queries.ts
+++ lib/sanity.queries.ts
@@ -1,69 +1,41 @@
 import { groq } from 'next-sanity';
 
-const postFields = groq`
-  _id,
-  title,
-  date,
-  excerpt,
-  coverImage,
-  content,
-  "slug": slug.current,
+export const homePageQuery = groq`
+  *[_type == "home"][0]{
+    _id,
+    overview,
+    showcaseProjects[]->{
+      _type,
+      coverImage,
+      overview,
+      "slug": slug.current,
+      tags,
+      title,
+    },
+    title,
+  }
 `;
 
-const projectFields = groq`
-  _id,
-  featured,
-  title,
-  coverImage,
-  description,
-  url,
-  technologies
+export const galleryPageQuery = groq`
+  *[_type == "gallery"]{
+      caption,
+      date,
+      image,
+  }
 `;
 
-const galleryFields = groq`
-  _id,
-  caption,
-  date,
-  "image": image.asset->url,
-  "dimensions": image.asset->metadata.dimensions
+export const pagesBySlugQuery = groq`
+  *[_type == "blog" && slug.current == $slug][0] {
+    _id,
+    body,
+    overview,
+    slug,
+    title,
+  }
 `;
 
-export const settingsQuery = groq`*[_type == "settings"][0]`;
-
-export const featuredBlogsQuery = groq`
-*[_type == "post"] | order(date desc, _updatedAt desc)  {
-  ${postFields}
-}`;
-
-export const featuredProjectsQuery = groq`
-*[_type == "project" && featured == true] {
-  ${projectFields}
-}`;
-
-export const allBlogsQuery = groq`
-{
-  "post": *[_type == "post"] | order(_updatedAt desc) [0] {
-    ${postFields}
-  },
-}`;
-
-export const allProjectsQuery = groq`
-*[_type == "project" ] {
-  ${projectFields}
-}`;
-
-export const postSlugsQuery = groq`
-*[_type == "post" && defined(slug.current)][].slug.current
-`;
-
-export const postBySlugQuery = groq`
-*[_type == "post" && slug.current == $slug][0] {
-  ${postFields}
-}
-`;
-
-export const galleryQuery = groq`
-*[_type == "gallery"] | order(date desc) {
-  ${galleryFields}
-}
+export const settingsQuery = groq`
+  *[_type == "settings"][0]{
+    ogImage,
+  }
 `;
