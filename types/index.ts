9971292c109ea65a4878b43a61d280f1f912b540
Mohamad Omran <mohamad.omran@postlight.com>
2023-04-26 00:46:49 +0300

    feat: add types for queries and pages

---
+++ types/index.ts
@@ -0,0 +1,62 @@
+import type { Image, PortableTextBlock } from 'sanity';
+
+export interface ShowcaseProject {
+  _type: string;
+  coverImage?: Image;
+  overview?: PortableTextBlock[];
+  site?: string;
+  tags?: string[];
+  title?: string;
+}
+
+export interface GalleryItem {
+  caption?: string;
+  date?: string;
+  image?: Image;
+}
+
+// Page payloads
+
+export interface HomePagePayload {
+  overview?: PortableTextBlock[];
+  showcaseProjects?: ShowcaseProject[];
+  title?: string;
+}
+
+export interface BlogPagePayload {
+  body?: PortableTextBlock[];
+  name?: string;
+  overview?: PortableTextBlock[];
+  title?: string;
+}
+
+export interface ProjectPayload {
+  client?: string;
+  coverImage?: Image;
+  description?: PortableTextBlock[];
+  duration?: {
+    start?: string;
+    end?: string;
+  };
+  overview?: PortableTextBlock[];
+  site?: string;
+  slug: string;
+  tags?: string[];
+  title?: string;
+}
+
+export interface SettingsPayload {
+  ogImage?: Image;
+}
+
+export interface GalleryPagePayload {
+  galleryItems?: GalleryItem[];
+}
+
+export interface GalleryPayload {
+  _type: string;
+  caption?: string;
+  date?: string;
+  image?: Image;
+}
+[];
