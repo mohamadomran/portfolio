d8f698241b71764613ae7379399a1cee52322412
Mohamad Omran <mohamad.omran@postlight.com>
2023-04-25 23:50:52 +0300

    feat: updated layout

---
+++ app/(personal)/layout.tsx
@@ -0,0 +1,25 @@
+// import 'styles/index.css';
+
+import { Footer } from '@/components/global/Footer';
+import { Header } from '@/components/global/Header';
+import { PreviewBanner } from '@/components/preview/PreviewBanner';
+import { getPreviewToken } from '@/lib/sanity.server.preview';
+
+export default async function IndexRoute({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const token = getPreviewToken();
+
+  return (
+    <div className="flex min-h-screen flex-col">
+      {token && <PreviewBanner />}
+      <Header />
+      <main className="mx-auto min-h-screen w-screen max-w-6xl px-10 py-5 sm:px-11 sm:py-6 md:px-12 md:py-0">
+        {children}
+      </main>
+      <Footer />
+    </div>
+  );
+}
