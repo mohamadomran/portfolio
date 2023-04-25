7265a14c3aca72076c0b5d1ffb1a5df85f677aaf
Mohamad Omran <mohamad.omran@postlight.com>
2023-04-26 01:49:51 +0300

    fix: project's preview

--- components/pages/projects/ProjectsPreview.tsx
+++ components/pages/projects/ProjectsPreview.tsx
@@ -2,12 +2,12 @@
 
 import { usePreview } from '@/lib/sanity.preview';
 import { projectsPageQuery } from '@/lib/sanity.queries';
-import type { GalleryItem, ProjectPayload } from '@/types';
+import type {  ProjectPayload } from '@/types';
 
 import { ProjectsPage } from './ProjectsPage';
 
 export function ProjectsPreview({ token }: { token: null | string }) {
-  const gallery: ProjectPayload = usePreview(token, projectsPageQuery);
+  const projects: ProjectPayload = usePreview(token, projectsPageQuery);
 
-  return <ProjectsPage images={gallery! as unknown as Array<GalleryItem>} />;
+  return <ProjectsPage projects={projects! as unknown as Array<ProjectPayload>} />;
 }
