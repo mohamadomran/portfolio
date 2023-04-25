fec241bb4b4a56eaa8eaa1cd5846dfbd0feceb8c
Mohamad Omran <mohamad.omran@postlight.com>
2023-04-25 23:31:48 +0300

    refactor: _document into app dir

---
+++ app/layout.tsx
@@ -0,0 +1,38 @@
+import 'tailwindcss/tailwind.css';
+
+export default async function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <link
+        rel="alternate"
+        type="application/rss+xml"
+        href="/rss/feed.xml"
+        title="RSS feed for mohamadomran.dev"
+      />
+      <link
+        rel="alternate"
+        type="application/feed+json"
+        href="/rss/feed.json"
+        title="RSS feed for mohamadomran.dev"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        href="/static/icons/favicon-16x16.png"
+        sizes="16x16"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        href="/static/icons/favicon-32x32.png"
+        sizes="32x32"
+      />
+      <link rel="apple-touch-icon" href="/static/icons/apple-touch-icon.png" />
+      <body>{children}</body>
+    </html>
+  );
+}
