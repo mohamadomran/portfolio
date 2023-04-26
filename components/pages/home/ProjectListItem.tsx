8abb1aea4f1a5a4131da30f43e4bf9e30147f4d3
Mohamad Omran <mohamad.omran@postlight.com>
2023-04-27 00:40:46 +0300

    style: featured project

--- components/pages/home/ProjectListItem.tsx
+++ components/pages/home/ProjectListItem.tsx
@@ -1,8 +1,9 @@
+import Image from 'next/image';
 import Link from 'next/link';
 
 import { ArrowRightIcon } from '@/components/icons/ArrowRightIcon';
 import { CustomPortableText } from '@/components/shared/CustomPortableText';
-import ImageBox from '@/components/shared/ImageBox';
+import { urlForImage } from '@/lib/sanity.image';
 import type { ShowcaseProject } from '@/types';
 
 interface ProjectProps {
@@ -13,27 +14,35 @@ interface ProjectProps {
 export function ProjectListItem(props: ProjectProps) {
   const { project } = props;
 
+  const imageUrl =
+    project.coverImage &&
+    urlForImage(project.coverImage)?.height(1200).width(2100).url();
   return (
     <>
       <h3 className="pt-6 text-3xl tracking-tight md:text-3xl">
         Featured Project
       </h3>
 
-      <div className="card-compact mt-8 overflow-hidden rounded-2xl bg-secondary-content">
-        <ImageBox
-          image={project.coverImage}
-          alt={`Cover image from ${project.title}`}
-          classesWrapper="rounded-2xl"
-        />
+      <div className="card mt-8 bg-secondary-content shadow-xl lg:card-side">
 
-        <div className="card-body mx-4">
+        <div className={`overflow-hidden rounded-2xl lg:w-[50%]`}>
+          {imageUrl && (
+            <Image
+              className="aspect-rectangle h-full w-full object-cover"
+              alt={project.title!}
+              width="2100"
+              height="1200"
+              src={imageUrl}
+            />
+          )}
+        </div>
+
+        <div className="card-body lg:w-[50%] ">
           <h2 className="card-title">{project.title!}</h2>
-
           <CustomPortableText value={project.overview!} />
-
-          <div className="card-actions mt-4 justify-end">
+          <div className="card-actions justify-end">
             <Link
-              className="btn-outline btn-primary btn"
+              className="btn-primary btn"
               href={project.site!}
               target="_blank"
             >
