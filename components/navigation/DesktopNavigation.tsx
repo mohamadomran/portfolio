e287fbf8565b1c5f64df5b9ffe9f99abfffd523c
Mohamad Omran <mohamad.omran@postlight.com>
2023-04-29 00:28:49 +0300

    fix: mobile header

--- components/navigation/DesktopNavigation.tsx
+++ components/navigation/DesktopNavigation.tsx
@@ -6,7 +6,7 @@ import { NavItem } from './NavItem';
 export function DesktopNavigation(props: Props) {
   return (
     <nav {...props}>
-      <ul className="flex rounded-2xl bg-base-100/70 px-4 text-lg  shadow-lg ring-1 ring-secondary backdrop-blur">
+      <ul className="flex rounded-2xl bg-base-100/70 text-sm md:text-lg shadow-lg ring-1 ring-secondary backdrop-blur">
         {pages.map(({ name, link }) => (
           <NavItem key={name} href={link}>
             {name}
