a02cdbabf2b0c7fd7159f1cbc2d8fcb242b3422b
Mohamad Omran <mohamad.omran@postlight.com>
2023-05-02 22:31:56 +0300

    feat: restore mobile nav

--- components/global/Header.tsx
+++ components/global/Header.tsx
@@ -1,11 +1,13 @@
 import { DesktopNavigation } from '../navigation/DesktopNavigation';
+import { MobileNavigation } from '../navigation/MobileNavigation';
 
 export function Header() {
   return (
     <>
       <header className="pointer-events-none relative sticky top-0 z-50">
-        <div className="top-0 z-10 flex h-16 justify-end justify-center pb-16 pt-6">
-          <DesktopNavigation className="pointer-events-auto block" />
+        <div className="top-0 z-10 flex h-16 justify-end md:justify-center pb-16 pt-6">
+          <DesktopNavigation className="pointer-events-auto hidden md:block" />
+          <MobileNavigation className="pointer-events-auto md:hidden pr-4" />
         </div>
       </header>
     </>
